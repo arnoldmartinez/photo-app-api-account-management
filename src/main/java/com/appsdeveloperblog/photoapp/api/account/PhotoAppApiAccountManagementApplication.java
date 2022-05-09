9240fa638f2829509e4e1c2bbfe2e6e1a8f9068b
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-09 17:36:39 -0500

    Give format to the code

--- src/main/java/com/appsdeveloperblog/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/account/PhotoAppApiAccountManagementApplication.java
@@ -11,5 +11,4 @@ public class PhotoAppApiAccountManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
     }
-
 }
